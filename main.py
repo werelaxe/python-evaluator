5f8da9f4c8b4fd71de10adef48f8dbe22d421f4e
melon <integrex@yandex.ru>
2017-07-19 03:32:47 +0500

    port fixes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import flag_server
 
 def main():
     with ProcessPoolExecutor(2) as task_handler:
-        task_handler.submit(flag_server.start_flag_server, 1234)
+        task_handler.submit(flag_server.start_flag_server, 31337)
         task_handler.submit(webserver.start_web_server)
 
 
